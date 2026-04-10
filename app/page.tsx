cd7b496fe2a79c92838a7b8e008016d3a33c504b
Abdulloh <abdullohmirzayev7571@gmail.com>
2026-04-10 14:42:48 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+	return (
+		<div className='flex flex-col items-center justify-center min-h-screen py-2'>
+			<h1 className='text-3xl font-bold'>Welcome to FEL-UP</h1>
+      <p className='text-2xl'>This is a project for the Frontend University Program</p>
+		</div>
+	)
+}
+
